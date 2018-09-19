ab260b35df9796b75264426075b4e181c98ef3aa
Chandan Rauniyar <chandankkrr@gmail.com>
2018-09-20 01:21:23 +1000

    Initial commit

---
+++ src/BioCard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function BioCard(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="headline" component="h2">
+          Chandan
+          {bull}
+          Rauniyar
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Software Developer
+        </Typography>
+        <Typography component="p">
+        🍭
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+BioCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BioCard);
